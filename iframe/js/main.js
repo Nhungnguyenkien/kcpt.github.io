06bc07c3543b0291658c3866893a45d50b35b614
Matthew Litwin <mlitwin@kcptech.com>
2015-04-07 12:48:52 -0700

    sketch path from url param

--- iframe/js/main.js
+++ iframe/js/main.js
@@ -1,8 +1,17 @@
 $(function() {
 
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(window.parent.document.location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var jsonPath = "./json/" + getParameterByName("sketch") + ".json";
+
 $(".sketch").
 		WSP("loadSketch", {
-			"data-url": "./json/logo.json",
+			"data-url": jsonPath,
 			"onReady": function(sketch, metadata) {
 				sketch.sQuery('[label="Animate"]').press();
 			}
