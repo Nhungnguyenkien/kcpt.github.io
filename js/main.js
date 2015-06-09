400ef971acff86480f8aa7b23f2a70014d496998
Matthew Litwin <mlitwin@kcptech.com>
2015-06-09 13:55:29 -0700

    add videos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(function() {
+
+$('#videoModal').on('show.bs.modal', function (event) {
+ var button = $(event.relatedTarget) 
+ var src = button.data("video-src");
+	
+ $(this).find("video").attr("src", src);
+
+});
+
+
+$('#videoModal').on('hide.bs.modal', function (event) { 
+ $(this).find("video").each(function() { this.pause(); });
+
+});
+
+})
 
