4ce1f8767ec1deb5a9438623b421ee5f254a73f1
Matthew Litwin <mlitwin@kcptech.com>
2016-03-16 14:45:14 -0700

    Add google analytics

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 $(function() {
 
 $('#videoModal').on('show.bs.modal', function (event) {
- var button = $(event.relatedTarget) 
+ var button = $(event.relatedTarget);
  var src = button.data("video-src");
  var title = button.find(".video-title").html();
-	
+
  $(this).find("video").attr("src", src);
  $(this).find(".modal-title").html(title);
 });
 
 
-$('#videoModal').on('hide.bs.modal', function (event) { 
+$('#videoModal').on('hide.bs.modal', function (event) {
  $(this).find("video").each(function() { this.pause(); });
 
 });
 
-})
+});
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75243707-1', 'auto');
+  ga('send', 'pageview');
